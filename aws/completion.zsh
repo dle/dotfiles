e02da195d5aec2c28970a1661b39f0ebc8dc4e68
Don Le <don@gametiime.com>
2014-04-16 11:13:28 -0700

    Add AWS

---
+++ aws/completion.zsh
@@ -0,0 +1,2 @@
+# AWS CLI autocomplete functions installed using Homebrew
+source /usr/local/share/zsh/site-functions/_aws
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/python:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:/usr/local/aws/api/bin:/usr/local/share/python:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
